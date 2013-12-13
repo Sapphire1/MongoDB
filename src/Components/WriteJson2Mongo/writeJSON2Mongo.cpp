59275b4b5ea69437c005062a93e97e32dbe20da8
lzmuda <lukzmuda1@gmail.com>
2013-12-13 21:42:22 +0100

    .

--- src/Components/WriteJson2Mongo/writeJSON2Mongo.cpp
+++ src/Components/WriteJson2Mongo/writeJSON2Mongo.cpp
@@ -15,44 +15,28 @@ TestDB_Processor::TestDB_Processor(const std::string & name) : Base::Component(n
                 m_thresh("thresh", 128, "range"),
                 m_maxval("maxval", 255, "range")
 {
-        LOG(LTRACE) << "Hello BarCode_Processor\n";
+        LOG(LTRACE) << "Hello Json2MongoWriter_Processor\n";
 
-        m_type.setToolTip("Thresholding type");
-        m_type.addConstraint("BINARY");
-        m_type.addConstraint("BINARY_INV");
-        m_type.addConstraint("TRUNC");
-        m_type.addConstraint("TOZERO");
-        m_type.addConstraint("TOZERO_INV");
-
-        m_thresh.setToolTip("Threshold level");
-        m_thresh.addConstraint("0");
-        m_thresh.addConstraint("255");
-
-        m_maxval.setToolTip("Maximum value to use with BINARY and BINARY_INV thresholding types");
-        m_maxval.addConstraint("0");
-        m_maxval.addConstraint("255");
-
-        // Register properties.
-        registerProperty(m_type);
-        registerProperty(m_thresh);
-        registerProperty(m_maxval);
-        try {
-         mongo::DBClientConnection c;
-                	c.connect("localhost");
-                   std::cout << "connected ok" << std::endl;
-                 } catch( const mongo::DBException &e ) {
-                   std::cout << "caught " << e.what() << std::endl;
-                 }
+        try
+        {
+        	mongo::DBClientConnection c;
+        	c.connect("localhost");
+            std::cout << "connected ok" << std::endl;
+         }
+         catch( const mongo::DBException &e )
+         {
+        	 std::cout << "caught " << e.what() << std::endl;
+         }
 }
 
 TestDB_Processor::~TestDB_Processor()
 {
-        LOG(LTRACE) << "Good bye BarCode_Processor\n";
+        LOG(LTRACE) << "Good bye Json2MongoWriter_Processor\n";
 }
 
 
 void TestDB_Processor::prepareInterface() {
-        CLOG(LTRACE) << "BarCode_Processor::prepareInterface\n";
+        CLOG(LTRACE) << "Json2MongoWriter_Processor::prepareInterface\n";
 
         h_onNewImage.setup(this, &TestDB_Processor::onNewImage);
         registerHandler("onNewImage", &h_onNewImage);
@@ -66,21 +50,21 @@ void TestDB_Processor::prepareInterface() {
 
 bool TestDB_Processor::onInit()
 {
-        LOG(LTRACE) << "BarCode_Processor::initialize\n";
+        LOG(LTRACE) << "Json2MongoWriter_Processor::initialize\n";
 
         return true;
 }
 
 bool TestDB_Processor::onFinish()
 {
-        LOG(LTRACE) << "TestDB_Processor_Processor::finish\n";
+        LOG(LTRACE) << "Json2MongoWriter_Processor_Processor::finish\n";
 
         return true;
 }
 
 bool TestDB_Processor::onStep()
 {
-        LOG(LTRACE) << "TestDB_Processor_Processor::step\n";
+        LOG(LTRACE) << "Json2MongoWriter_Processor_Processor::step\n";
         return true;
 }
 
@@ -91,13 +75,12 @@ bool TestDB_Processor::onStop()
 
 bool TestDB_Processor::onStart()
 {
-	std::cout<<"STRTTTTTTTTTTTTTTTTTTTTTT\n";
         return true;
 }
 
 void TestDB_Processor::onNewImage()
 {
-        LOG(LNOTICE) << "TestDB_Processor::onNewImage\n";
+        LOG(LNOTICE) << "Json2MongoWriter_Processor::onNewImage\n";
         try {
                 cv::Mat img = in_img.read();
                 cv::Mat out = img.clone();
@@ -105,7 +88,7 @@ void TestDB_Processor::onNewImage()
                 cv::threshold(img, out, m_thresh, m_maxval, m_type);
                 out_img.write(out);
         } catch (...) {
-                LOG(LERROR) << "TestDB_Processor::onNewImage failed\n";
+                LOG(LERROR) << "Json2MongoWriter_Processor::onNewImage failed\n";
         }
 
 }
